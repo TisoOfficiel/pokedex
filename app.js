0cbcf3c3a299355f15f09e76d8d392075928827a
Romain Lhuillier <51294979+TisoOfficiel@users.noreply.github.com>
2022-09-11 17:37:06 +0200

    update : with input 3D

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-function seach(data){
-    var test = fetch('https://pokeapi.co/api/v2/pokemon/'+data)
+var input = document.getElementById("data");
+   input.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {  
+      search(input.value);
+    }
+  });
+
+
+function search(data) {
+    var url = fetch('https://pokeapi.co/api/v2/pokemon/'+data)
     .then(response => response.json())
-    .then(function(pokeData){
-        document.getElementById('name').innerHTML = pokeData.name
+        .then(function (pokeData) {
+        console.log(pokeData)
+        document.getElementById('name').innerHTML = pokeData.name+" #"+pokeData.id
         document.getElementById('pokemon').src = pokeData['sprites']['front_default']
     })
+
+
+
+
+
+}
+
+function clear() {
+    document.getElementsById('data').value = "";
 }

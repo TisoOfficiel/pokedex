8187d91b72c3224ceebd6064e0478e951c5c1c9f
Romain Lhuillier <romainlhuillier94@gmail.com>
2022-09-13 22:32:16 +0200

    add pokemon type

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ var input = document.getElementById("data");
       var url = await fetch('https://pokeapi.co/api/v2/pokemon/'+data)
       .then(response => response.json())
           .then(function (pokeData) {
+            console.log(pokeData.types[0]['type']['name']);
+            console.log(pokeData.types[1]['type']['name']);
+            document.getElementById('data').value = ""
             document.getElementById('name').dataset.id = ""
             document.getElementById('pokemon').src=""
             document.getElementById('height').innerHTML=""
@@ -30,7 +33,7 @@ var input = document.getElementById("data");
           document.getElementById('name').dataset.id = pokeData.id
           // Take the img of the pokemon
           document.getElementById('pokemon').src = pokeData['sprites']['front_default']
-          //Take the height &é weight of the pokemon
+          //Take the height & weight 
           document.getElementById('height').innerHTML= "Height: "+pokeData.height
           document.getElementById('weight').innerHTML="Weight: "+pokeData.weight
 
@@ -41,17 +44,81 @@ var input = document.getElementById("data");
           document.getElementById('special-attack').innerHTML="s-att : "+pokeData['stats'][3]['base_stat']
           document.getElementById('special-defence').innerHTML="s-def : "+pokeData['stats'][4]['base_stat']
           document.getElementById('speed').innerHTML="speed : "+pokeData['stats'][5]['base_stat']
-      })
+      
+          // type
+          // typePokemon(pokeData.types[0]['type']['name'])
+
+          // typePokemon(pokeData.types[1]['type']['name'])
+          
+          img
+      
+      
+        })
     } catch (error) {
       
     }
 }
 
+function typePokemon(type){
+  switch (pokeData) {
+    case "bug":
+      
+      return "./assets/type_pokemon/bug.png"
+    case "dark":
+      return "assets\type_pokemon\dark.png"
+    case "dragon":
+      return "assets\type_pokemon\dragon.png"
+    case "electric":
+      return "assets\type_pokemon\electr.png"
+    case "fairy":
+      return "assets\type_pokemon\fairy.png"
+    case "fighting":
+      return "assets\type_pokemon\fight.png"
+    case "fire":
+      return "assets\type_pokemon\fire.png"
 
+    case "flying":
+      return ""
+    case "ghost":
+      return ""
+    case "ground":
+      return ""
+    case "grass":
+      return ""
+    case "ice":
+      return ""
+    case "normal":
+      return ""
+    case "poison":
+      return ""
+    case "psychic":
+      return ""
+    case "rock":
+      return ""
+    case "steel":
+      return ""
+    case "water":
+      return "" 
+    
+    
+    
+   
+   
+    
+   
+   
+    
+   
+   
+   
+  }
+}
 function previous(){
   var id = document.getElementById('name').dataset.id
-  id--
-  search(id)
+  if(id > 1){
+    id--
+    search(id)
+  }
 }
 
 function next(){
